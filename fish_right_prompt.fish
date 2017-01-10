5d4369fa90b45707ae307ea1c678904674398b29
travisbartholome <travis.bartholome@gmail.com>
2017-01-09 18:05:52 -0600

    Suppressed errors in fish_right_prompt; git diff-index would error while in /.git/

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,12 +1,12 @@
 function fish_right_prompt -d "Write the right-side prompt"
   # Color-coded Git changes - form is "2|8+|3-"
   if git_is_repo
-    if test -z (git diff-index --shortstat HEAD)
+    if test -z (git diff-index --shortstat HEAD 2>/dev/null)
       return
     end
-    set -l git_changes (git diff-index --shortstat HEAD | grep -o '[0-9]\{1,\} file' | cut -d' ' -f1)
-    set -l git_additions (git diff-index --shortstat HEAD | grep -o '[0-9]\{1,\} insertion' | cut -d' ' -f1)
-    set -l git_deletions (git diff-index --shortstat HEAD | grep -o '[0-9]\{1,\} deletion' | cut -d' ' -f1)
+    set -l git_changes (git diff-index --shortstat HEAD  2>/dev/null | grep -o '[0-9]\{1,\} file' | cut -d' ' -f1)
+    set -l git_additions (git diff-index --shortstat HEAD  2>/dev/null | grep -o '[0-9]\{1,\} insertion' | cut -d' ' -f1)
+    set -l git_deletions (git diff-index --shortstat HEAD  2>/dev/null | grep -o '[0-9]\{1,\} deletion' | cut -d' ' -f1)
     set_color AAA
     echo -n $git_changes
     if not test -z $git_additions
